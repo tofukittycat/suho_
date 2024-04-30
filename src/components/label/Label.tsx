19068773eb8efadbdd4df85112ad824af9df2a7e
FSL-Buzz <buzz.kim@forspacelab.com>
2024-04-30 17:40:22 +0900

    feat: Label Component 작업

---
+++ src/components/label/Label.tsx
@@ -0,0 +1,59 @@
+import { ReactNode } from "react";
+
+import { Label as LabelUI } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+
+type LabelType = "Title1" | "Title2" | "SubTitle1" | "SubTitle2" | "Body1" | "Body2" | "Caption";
+
+type LabelProps = {
+  type?: LabelType;
+  className?: string;
+  children: ReactNode;
+};
+
+export default function Label({ type, className, children }: LabelProps) {
+  switch (type) {
+    case "Title1":
+      return (
+        <LabelUI className={cn("font-pretendard text-[28px] font-bold", className)}>
+          {children}
+        </LabelUI>
+      );
+    case "Title2":
+      return (
+        <LabelUI className={cn("font-pretendard text-2xl font-semibold", className)}>
+          {children}
+        </LabelUI>
+      );
+    case "SubTitle1":
+      return (
+        <LabelUI className={cn("font-pretendard text-xl font-bold", className)}>{children}</LabelUI>
+      );
+    case "SubTitle2":
+      return (
+        <LabelUI className={cn("font-pretendard text-lg font-semibold", className)}>
+          {children}
+        </LabelUI>
+      );
+    case "Body1":
+      return (
+        <LabelUI className={cn("font-pretendard text-base font-normal", className)}>
+          {children}
+        </LabelUI>
+      );
+    case "Body2":
+      return (
+        <LabelUI className={cn("font-pretendard text-sm font-normal", className)}>
+          {children}
+        </LabelUI>
+      );
+    case "Caption":
+      return (
+        <LabelUI className={cn("font-pretendard text-xs font-normal", className)}>
+          {children}
+        </LabelUI>
+      );
+    default:
+      return <LabelUI className={cn("font-pretendard", className)}>{children}</LabelUI>;
+  }
+}
