0ab51dbb033b85e3ec415194e669b10128234738
FSL-Buzz <buzz.kim@forspacelab.com>
2024-05-09 21:30:39 +0900

    feat: 로그인 후 추가 정보 입력 UI 개발

--- src/app/(login)/signin/page.tsx
+++ src/app/(login)/signin/page.tsx
@@ -4,20 +4,20 @@ import VCStack from "@/components/base/stack/VCStack";
 import VStack from "@/components/base/stack/VStack";
 import { Button } from "@/components/ui/button";
 
-import LoginButton from "./components/LoginButton";
-import useSignIn from "./hooks/useSignIn";
+import LoginButton from "./@components/LoginButton";
+import useSignIn from "./@hooks/useSignIn";
 
 export default function page() {
-  const { handleGoToSignUp } = useSignIn();
+  const { handleGoToSignUp, handleGoToEmail } = useSignIn();
 
   return (
     <VStack wFull hFull className="gap-[12px] px-[20px]">
       <VCStack className=" mt-[40px] h-[220px] rounded-[26px] bg-[#F2F4F5]">Lottie</VCStack>
       <VStack className="gap-[7px]">
-        <LoginButton type="KAKAO" />
-        <LoginButton type="NAVER" />
-        <LoginButton type="GOOGLE" />
-        <LoginButton type="EMAIL" />
+        <LoginButton buttonType="KAKAO" />
+        <LoginButton buttonType="NAVER" />
+        <LoginButton buttonType="GOOGLE" />
+        <LoginButton buttonType="EMAIL" onClick={handleGoToEmail} />
       </VStack>
       <Button
         variant={"ghost"}
