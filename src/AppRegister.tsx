813bfab82ffa0d40ca72f0c241ec32d22e1f1084
FSL-Buzz <buzz.kim@forspacelab.com>
2024-05-25 00:14:42 +0900

    모바일 사이즈 테스트

--- src/AppRegister.tsx
+++ src/AppRegister.tsx
@@ -9,21 +9,12 @@ import ReactQueryProvider from "./providers/react-query/ReactQueryProvider";
 import RecoilProvider from "./providers/recoil/RecoilProvider";
 import { ThemeProvider } from "./providers/shacnd/ThemeProvider";
 import ApiErrorBoundary from "./services/ApiErrorBoundary";
-import setMobileHeight from "./utils/mobileSize";
 
 type AppRegisterProps = {
   children: ReactNode;
 };
 
 export default function AppRegister({ children }: AppRegisterProps) {
-  useEffect(() => {
-    // 모바일 Height 적용
-    setMobileHeight();
-
-    window.addEventListener("resize", setMobileHeight);
-    return () => window.removeEventListener("resize", setMobileHeight);
-  }, []);
-
   return (
     <RecoilProvider>
       <ThemeProvider>
--- src/utils/mobileSize.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 모바일 vh 이슈 대응
- */
-export default function setMobileHeight() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-}
