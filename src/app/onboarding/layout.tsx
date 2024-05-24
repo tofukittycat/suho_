5726b5a9cdf96158eb0ed4c495124fedd924f533
FSL-Buzz <buzz.kim@forspacelab.com>
2024-05-24 23:27:43 +0900

    모바일 사이즈 테스트

--- src/app/onboarding/layout.tsx
+++ src/app/onboarding/layout.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from "react";
-import MobileViewLayout from "react-mobile-layout";
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 export default function layout({ children }: LayoutProps) {
-  // return <div className="h-full w-full">{children}</div>;
-  return <MobileViewLayout>{children}</MobileViewLayout>;
+  return <div className="h-full w-full">{children}</div>;
 }
