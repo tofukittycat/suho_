f95f649d1bf8516a5ccdc932b922aa7d727faf4d
FSL-Buzz <buzz.kim@forspacelab.com>
2024-06-18 03:15:44 +0900

    fix: 행운 부적 생성시 리프레시 해야 걸리는 이슈 수정

--- src/app/luckytree/_hooks/queries/useMutateRemoveTree.tsx
+++ src/app/luckytree/_hooks/queries/useMutateRemoveTree.tsx
@@ -1,9 +1,17 @@
 import { deleteTree } from "@/services/luckytree";
-import { useMutation } from "@tanstack/react-query";
+import { QueryKeys } from "@/services/queryKeys";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function useMutateRemoveTree() {
+  const queryClient = useQueryClient();
+
   const { mutate, isPending } = useMutation({
     mutationFn: ({ treeId }: { treeId: number }) => deleteTree({ treeId }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QueryKeys.TreeInfo],
+      });
+    },
   });
 
   return {
