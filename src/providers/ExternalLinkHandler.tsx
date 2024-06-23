374f28f0de74f60bad5a90bea660642b049baaa2
jinShine <seungjin429@gmail.com>
2024-06-23 13:15:58 +0900

    feat: 인앱브라우저일때 외부에서 열리게 로직 추가

--- src/providers/ExternalLinkHandler.tsx
+++ src/providers/ExternalLinkHandler.tsx
@@ -9,27 +9,28 @@ export default function ExternalLinkHandler() {
   const [lastPath, setLastPath] = useState("");
 
   useEffect(() => {
-    if (pathname !== lastPath) {
-      handleRouteChange(pathname);
-      setLastPath(pathname);
-    }
-  }, [pathname, lastPath]);
+    handleRouteChange(pathname);
+  }, []);
 
   const handleRouteChange = (url: string) => {
     const userAgent = navigator.userAgent;
 
-    if (/KAKAOTALK/i.test(userAgent)) {
+    if (
+      /inapp|naver|snapchat|wirtschaftswoche|thunderbird|instagram|everytimeapp|whatsApp|electron|wadiz|aliapp|zumapp|iphone(.*)whale|android(.*)whale|kakaostory|band|twitter|DaumApps|DaumDevice\/mobile|FB_IAB|FB4A|FBAN|FBIOS|FBSS|trill|SamsungBrowser\/[^1]/i.test(
+        userAgent,
+      )
+    ) {
       if (/android/i.test(userAgent)) {
         window.location.href = `intent://${url.replace(/^https?:\/\//, "")}#Intent;scheme=http;package=com.android.chrome;end`;
       } else if (/iPhone|iPad|iPod/i.test(userAgent)) {
-        window.location.href = `googlechrome://${url.replace(/^https?:\/\//, "")}`;
-
+        // window.location.href = `googlechrome://${url.replace(/^https?:\/\//, "")}`;
+        window.location.href = url;
         // setTimeout(() => {
         //   alert("This function is not supported in KakaoTalk in-app browser.");
         // }, 500);
       }
     } else {
-      window.location.href = url;
+      // window.location.href = url;
     }
   };
 
